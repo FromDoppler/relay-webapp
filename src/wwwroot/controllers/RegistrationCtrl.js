de71b8200714fe8e13399f7ca7af90a519e53684
mvillaseco <mvillaseco@makingsense.com>
2017-11-17 16:32:05 -0300

    [DR-1461] Change regular captcha to invisible captcha in registration

--- src/wwwroot/controllers/RegistrationCtrl.js
+++ src/wwwroot/controllers/RegistrationCtrl.js
@@ -23,6 +23,9 @@
     vm.submitRegistration = submitRegistration;
     vm.emailRegistered = null;
     vm.regexAllowedAccountName = /^[a-z-0-9_-]*$/;
+    vm.setCaptchaResponse = setCaptchaResponse;
+    vm.setWidgetId = setWidgetId;
+    vm.reloadCaptcha = reloadCaptcha;
 
     var customAccountName = false;
     vm.accountNameUpdated = function () {
@@ -40,7 +43,38 @@
       if (form.$invalid) {
         return;
       }
+      vcRecaptchaService.execute(vm.widgetId);      
+    }
 
+    var onExpectedError = function (rejectionData) {
+      var handled = false;
+      if (vm.recaptchaAvailable){
+        recaptcha.reload();
+      }
+      var accountNameError = rejectionData.errors.find(function (error) {
+        return error.key == "account_name";
+      });
+      if (accountNameError) {
+        handled = true;
+        utils.setServerValidationToField($scope, $scope.form.accountName, 'accountname_already_taken');
+      }
+
+      var emailError = rejectionData.errors.find(function (error) {
+        return error.key == "user_email";
+      });
+      if (emailError) {
+        handled = true;
+        utils.setServerValidationToField($scope, $scope.form.email, 'email_already_exist');
+      }
+
+      return handled;
+    };
+
+    function setWidgetId (widgetId) {
+      vm.widgetId = widgetId;
+    };
+
+    function setCaptchaResponse(response) {
       var user = {
         user_email: vm.email,
         firstName: vm.firstName,
@@ -49,38 +83,22 @@
         account_name: vm.accountName,
         company: vm.company,
         termsAndConditions: vm.checkTerms ? $rootScope.getTermsAndConditionsVersion() : null,
-        recaptchaResponse: form.recaptchaResponse
-      };
-
-      var onExpectedError = function (rejectionData) {
-        var handled = false;
-        if (vm.recaptchaAvailable){
-          recaptcha.reload();
-        }
-        var accountNameError = rejectionData.errors.find(function (error) {
-          return error.key == "account_name";
-        });
-        if (accountNameError) {
-          handled = true;
-          utils.setServerValidationToField($scope, $scope.form.accountName, 'accountname_already_taken');
-        }
-
-        var emailError = rejectionData.errors.find(function (error) {
-          return error.key == "user_email";
-        });
-        if (emailError) {
-          handled = true;
-          utils.setServerValidationToField($scope, $scope.form.email, 'email_already_exist');
-        }
-
-        return handled;
+        recaptchaResponse: response
       };
 
       signup.registerUser(user, $translate.use(), onExpectedError)
-        .then(function (result) {
-          $location.path('/signup/succeed').search({'email': vm.email});
-        });
-
+      .then(function (result) {
+        $location.path('/signup/succeed').search({'email': vm.email});
+      }).catch(function (result){
+        vm.forgotSuccessful = false;
+      }).finally(function (){
+        reloadCaptcha();
+      });
     }
+
+    function reloadCaptcha () {
+      vcRecaptchaService.reload(vm.widgetId);
+      vm.response = null;
+    };
   }
 })();
