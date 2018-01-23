cbb24d70330cdf41ed128fd182f8c5f38bbfa999
Andres Moschini <andresmoschini@gmail.com>
2018-01-23 12:27:00 -0300

    DKIM status can take up to 24 hours to update

--- src/wwwroot/locales/en-translation.js
+++ src/wwwroot/locales/en-translation.js
@@ -447,7 +447,7 @@
   "login_error_invalid_account_impersonate": "Account not valid to impersonate",
   "dkim_not_ready_header_text": "Your account is still inactive, to start using it you must verify your domain by configuring your DKIM.",
   "dkim_not_ready_header_button": "Verify domain",
-  "domain_manager_remember_hours_update": "Remember that the DKIM status can take up to 6 hours to update.",
+  "domain_manager_remember_hours_update": "Remember that the DKIM status can take up to 24 hours to update.",
   "dkim_config_help_href": "en/how-to-configure-your-dkim-step-by-step/",
   "domain_manager": "Domain Manager",
   "dkim_configuration_help": "Domain Settings"
