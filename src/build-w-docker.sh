59c3c598002cdfafb37121a967326f2a2db2a1cf
Andrés Moschini <amoschini@makingsense.com>
2018-06-15 13:59:31 -0300

    Add docker scripts

---
+++ src/build-w-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+pkgName="relay-webapp"
+pkgVersion=${1:-"v0.0.0-build0"}
+cdnBaseUrl=${2:-"//cdn.fromdoppler.com/$pkgName"}
+cdnUrl="$cdnBaseUrl/$pkgVersion"
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Lines added to get the script running in the script path shell context
+# reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
+cd $(dirname $0)
+
+rm -rf `pwd`/build && mkdir `pwd`/build
+docker run --rm \
+    -v /`pwd`:/work \
+    -w /work \
+    node:6.10.1 \
+    /bin/sh -c "\
+        yarn global add gulp \
+        && yarn \
+        && gulp build \
+        && gulp test \
+    "
