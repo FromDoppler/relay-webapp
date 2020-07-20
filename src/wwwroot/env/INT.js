0853229a57de5867595b35316e696df801bdd4f3
Victor Popovich <victhor.popovich@gmail.com>
2020-07-20 18:05:11 -0300

    chore: add cuit service by env

--- src/wwwroot/env/INT.js
+++ src/wwwroot/env/INT.js
@@ -1,5 +1,6 @@
 ﻿angular.module('dopplerRelay').constant('RELAY_CONFIG', {
     baseUrl: 'https://dopplerrelayint.makingsense.com:4443',
+    cuitServiceBaseUrl: 'https://cuitserviceint.fromdoppler.net',
     hostSmtp: 'dopplerrelayint.makingsense.com',
     portSmtp: '2525'
 });
--- src/wwwroot/env/PROD.js
+++ src/wwwroot/env/PROD.js
@@ -1,5 +1,6 @@
 ﻿angular.module('dopplerRelay').constant('RELAY_CONFIG', {
   baseUrl: 'https://api.dopplerrelay.com',
+  cuitServiceBaseUrl: 'https://cuitservice.fromdoppler.com',
   hostSmtp: 'smtp.dopplerrelay.com',
   portSmtp: '587'
 });
