eb8cab727bed892e90bf26549bce3c820f51cbb5
Victor Popovich <victhor.popovich@gmail.com>
2020-07-28 14:51:22 -0300

    chore: update cuit service qa environment

--- src/wwwroot/env/QA.js
+++ src/wwwroot/env/QA.js
@@ -1,6 +1,6 @@
 ﻿angular.module('dopplerRelay').constant('RELAY_CONFIG', {
   baseUrl: 'https://dopplerrelayqa.makingsense.com:4443',
-  cuitServiceBaseUrl: 'https://cuitserviceqa.fromdoppler.net',
+  cuitServiceBaseUrl: 'https://apisqa.fromdoppler.com/cuit',
   hostSmtp: 'dopplerrelayqa.makingsense.com',
   portSmtp: '2525'
 });
