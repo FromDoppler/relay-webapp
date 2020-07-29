0402cd171454c6168c8365b62ecc51e038658e12
Victor Popovich <victhor.popovich@gmail.com>
2020-07-29 09:36:21 -0300

    chore: use prettier code formatter

--- src/wwwroot/env/INT.js
+++ src/wwwroot/env/INT.js
@@ -1,6 +1,6 @@
-﻿angular.module('dopplerRelay').constant('RELAY_CONFIG', {
-  baseUrl: 'https://dopplerrelayint.makingsense.com:4443',
-  cuitServiceBaseUrl: 'https://apisint.fromdoppler.com/cuit',
-  hostSmtp: 'dopplerrelayint.makingsense.com',
-  portSmtp: '2525'
+﻿angular.module("dopplerRelay").constant("RELAY_CONFIG", {
+  baseUrl: "https://dopplerrelayint.makingsense.com:4443",
+  cuitServiceBaseUrl: "https://apisint.fromdoppler.com/cuit",
+  hostSmtp: "dopplerrelayint.makingsense.com",
+  portSmtp: "2525",
 });
