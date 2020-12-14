3596fd7a5463f1a5000398a11b2719745a00c637
Martin Smionati <msimionati@makingsense.com>
2020-12-14 11:51:01 -0300

    fix: chage url to cuit service

--- src/wwwroot/env/QA.js
+++ src/wwwroot/env/QA.js
@@ -1,6 +1,6 @@
 ﻿angular.module("dopplerRelay").constant("RELAY_CONFIG", {
   baseUrl: "https://dopplerrelayqa.makingsense.com:4443",
-  cuitServiceBaseUrl: "https://apisqa.fromdoppler.com/cuit",
+  cuitServiceBaseUrl: "https://apisqa.fromdoppler.net/cuit",
   hostSmtp: "dopplerrelayqa.makingsense.com",
   portSmtp: "2525",
 });
