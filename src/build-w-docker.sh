2205c14e9bcf4c19fcd4c8a1a1a7647419f7b1b1
Claudio Rodrigo Pereyra Diaz <cpereyra@makingsense.com>
2021-05-21 09:35:56 -0300

    chore: avoid issues with MINGW and Git Bash in sh scripts

--- src/build-w-docker.sh
+++ src/build-w-docker.sh
@@ -12,6 +12,12 @@ set -e
 # reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
 cd $(dirname $0)
 
+# To avoid issues with MINGW and Git Bash, see:
+# https://github.com/docker/toolbox/issues/673
+# https://gist.github.com/borekb/cb1536a3685ca6fc0ad9a028e6a959e3
+export MSYS_NO_PATHCONV=1
+export MSYS2_ARG_CONV_EXCL="*"
+
 rm -rf `pwd`/build && mkdir `pwd`/build
 docker run --rm \
     -v /`pwd`:/work \
--- src/run-w-docker.sh
+++ src/run-w-docker.sh
@@ -7,6 +7,12 @@ set -e
 # reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
 cd $(dirname $0)
 
+# To avoid issues with MINGW and Git Bash, see:
+# https://github.com/docker/toolbox/issues/673
+# https://gist.github.com/borekb/cb1536a3685ca6fc0ad9a028e6a959e3
+export MSYS_NO_PATHCONV=1
+export MSYS2_ARG_CONV_EXCL="*"
+
 docker run --rm \
     -v /`pwd`:/work \
     -p 3000:3000 \
