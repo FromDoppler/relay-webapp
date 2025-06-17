0659902c373c089997146176468efa5c0b464905
homecavs <homecavs@gmail.com>
2025-06-17 15:08:28 -0300

    chore: omit shellcheck verification

--- src/build-n-publish.sh
+++ src/build-n-publish.sh
@@ -11,7 +11,7 @@ set -e
 
 # Lines added to get the script running in the script path shell context
 # reference: http://www.ostricher.com/2014/10/the-right-way-to-get-the-directory-of-a-bash-script/
-# shellcheck disable=SC2046, SC2086
+# shellcheck disable=SC2046,SC2086
 cd $(dirname $0)
 
 # To avoid issues with MINGW and Git Bash, see:
@@ -26,7 +26,7 @@ export MSYS2_ARG_CONV_EXCL="*"
 # Force pull the latest image version due to the cache not always is pruned immediately after an update is uploaded to docker hub
 docker pull dopplerrelay/doppler-relay-akamai-publish
 
-# shellcheck disable=SC2046, SC2006
+# shellcheck disable=SC2046,SC2006
 docker run --rm \
     -e AKAMAI_CDN_HOSTNAME \
     -e AKAMAI_CDN_USERNAME \
