da801766dfef6dc117c2754104a1c6b5c7a46d9f
homecavs <homecavs@gmail.com>
2026-07-21 13:19:47 -0300

    style: fix iframe height

--- src/wwwroot/services/eprotect.js
+++ src/wwwroot/services/eprotect.js
@@ -92,7 +92,7 @@
         reportGroup: RELAY_CONFIG.eprotectReportGroup,
         timeout: '5000',
         div: divId,
-        height: '230',
+        height: '350',
         callback: callback,
         showCvv: true,
         months: months,
