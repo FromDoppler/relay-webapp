3b7b6be6c0a36be688dad21c40933d5ac3abe80b
fgonzalez <fgonzalez@makingsense.com>
2026-08-20 15:43:32 -0300

    chore: add optional configs

--- src/wwwroot/template-editor/adapterService.js
+++ src/wwwroot/template-editor/adapterService.js
@@ -495,7 +495,16 @@
         rssCampaign: false,
         rssShowPreview: false,
         fileServerImagesURL: "https://app2.dopplerfiles.com",
-        socialImagesURL: "https://cdn.fromdoppler.com/unlayer-editor/assets/socialNetworkIcons/"
+        socialImagesURL: "https://cdn.fromdoppler.com/unlayer-editor/assets/socialNetworkIcons/",
+        abandonedCartCampaign: false,
+        visitedProductsCampaign: false,
+        bestSellingEnabled: false,
+        newProductsEnabled: false,
+        crossSellingEnabled: false,
+        pendingOrderCampaign: false,
+        confirmationOrderCampaign: false,
+        promotionCodeEnabled: false,
+        textTool: null
       };
       return $q.resolve(msEditorSettings);
     }
